b9d491c3b7093390ec48125b7030170e30c3f37d
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-02 21:49:49 +0700

    synaptics_dsx: Update key enabler value when really suspend/resume
    Change-Id: Icd2247d685592eb738678d3d789a38990b7603ae

--- drivers/input/touchscreen/synaptics_dsx_xiaomi/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_xiaomi/synaptics_dsx_core.c
@@ -4757,9 +4757,6 @@ static int synaptics_rmi4_suspend(struct device *dev)
 		if (rmi4_data->suspend)
 			return 0;
 
-		/* Ketut P. Kumajaya: Revert button enabler on suspend */
-		synaptics_rmi4_0dbutton_update(rmi4_data, !rmi4_data->button_0d_enabled);
-
 		if (rmi4_data->enable_wakeup_gesture) {
 			synaptics_rmi4_wakeup_gesture(rmi4_data, true);
 			synaptics_rmi4_free_fingers(rmi4_data);
@@ -4819,9 +4816,6 @@ static int synaptics_rmi4_resume(struct device *dev)
 		schedule_delayed_work(&rmi4_data->resume_delayed_work,
 				msecs_to_jiffies(bdata->reset_delay_ms));
 	} else {
-		/* Ketut P. Kumajaya: Revert button enabler on resume */
-		synaptics_rmi4_0dbutton_update(rmi4_data, !rmi4_data->button_0d_enabled);
-
 		if (rmi4_data->enable_wakeup_gesture)
 			synaptics_rmi4_wakeup_gesture(rmi4_data, false);
 		else {
